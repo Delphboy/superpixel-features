842640539e9091efe33b9ed407d369ffd29ddd0b
Henry <hps1945@live.co.uk>
2023-12-17 14:19:32 +0000

    add hpc support and COCO file renaming

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 import os
 
 import numpy as np
@@ -6,16 +7,32 @@ import torch
 import torch.nn as nn
 from torchvision.models.resnet import ResNet101_Weights, resnet101
 
-from superpixel_features import (
-    get_features_using_superpixels,
-    get_superpixels,
-    load_image,
-)
+from superpixel_features import (get_features_using_superpixels,
+                                 get_superpixels, load_image)
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 MODEL = None
 
+LOGGER = None
+
+def get_logger(save_dir: str):
+    global LOGGER
+    if LOGGER is None:
+        # set up logger
+        logging.basicConfig(level=logging.INFO)
+        # set logging file to log.txt
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.INFO)
+        handler = logging.FileHandler(f"log-{save_dir.split('/')[-1]}.txt")
+        handler.setLevel(logging.INFO)
+        formatter = logging.Formatter("%(message)s")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        LOGGER = logger
+    return LOGGER
+
+logging.info("Device: {}".format(DEVICE))
 
 def get_model():
     global MODEL
@@ -28,7 +45,7 @@ def get_model():
 
 
 def process(
-    image_dir: str, output_dir: str, superpixel_algorithm: str, num_superpixels: int
+    image_dir: str, output_dir: str, num_superpixels: int
 ):
     # if output_dir does not exist, create it
     if not os.path.exists(output_dir):
@@ -37,7 +54,7 @@ def process(
     # Get the images in the directory
     images = os.listdir(image_dir)
     for i, image in enumerate(images):
-        print(f"{i+1}/{len(images)} | Processing image: {image}")
+        LOGGER.info(f"{i+1}/{len(images)} | Processing image: {image}")
         scikit_image, torch_image = load_image(os.path.join(image_dir, image))
         superpixels = get_superpixels(
             img_scikit=scikit_image, n_segments=num_superpixels
@@ -71,9 +88,10 @@ if __name__ == "__main__":
 
     args = args.parse_args()
 
+    get_logger(args.save_dir)
+
     process(
         image_dir=args.image_dir,
         output_dir=args.save_dir,
-        superpixel_algorithm=args.superpixel_algorithm,
         num_superpixels=args.num_superpixels,
     )
