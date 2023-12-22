a38a2ee917aa9e2473b9b7a076e52a1f8b9844cb
Henry <hps1945@live.co.uk>
2023-12-22 15:13:04 +0000

    batch support on qsub

--- submitter.sh
+++ submitter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 declare sets=("train" "val" "test")
-declare sizes=(25 50 75 100)
+declare sizes=(5 10 15 30 35 60 80)
 
 
 for set in "${sets[@]}"
@@ -9,8 +9,9 @@ do
     for size in "${sizes[@]}"
     do
         echo "Processing $set with $size superpixels"
-        sbatch --export=SET=$set,SIZE=$size submit.sh --job-name=$set-$size --output=/jmain02/home/J2AD007/txk47/hxs67-txk47/superpixel-features/$set-$size.out submit.sh
+        # sbatch --export=SET=$set,SIZE=$size submit.sh --job-name=$set-$size --output=/jmain02/home/J2AD007/txk47/hxs67-txk47/superpixel-features/$set-$size.out submit.sh
+        qsub -v SET=$set,SIZE=$size -N $set-$size submit.qsub
     done
 done
 
-squeue -u $(whoami)
+# squeue -u $(whoami)
